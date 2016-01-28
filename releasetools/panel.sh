24acde9d57dd1b9b664662a38f41cc25423e6490
Zdrowy Gosciu <ZdrowyGosciu+GITHUB@gmail.com>
2016-01-28 17:20:43 +0100

    g2-common: releasetools: panel.sh: Update panel detection + correct maxfreq value

--- releasetools/panel.sh
+++ releasetools/panel.sh
@@ -1,6 +1,6 @@
 #!/sbin/sh
 #
-# Panel Detection Script - Authored by dr87 & maintained by Rashed97
+# Panel Detection Script - Authored by dr87 & maintained by Rashed97 feat. Zdrowy Gosciu
 #
 # Detect panel and swap as necessary 
 # lcd_maker_id is determined by get_panel_maker_id on the hardware and is always accurate
@@ -10,21 +10,26 @@
 #	LCD_RENESAS_LGD = 0,
 #	LCD_RENESAS_JDI = 1
 #
+# Look for qcom,dsi-pref-prim-pan in DTS
+#
 
 lcdmaker=$(grep -c "lcd_maker_id=1" /proc/cmdline)
-if [ $lcdmaker == 1 ]; then
-	echo "JDI panel detected"
+if [ "$lcdmaker" == "1" ]; then
+	echo "JDI panel detected.. Swapping.."
 	find /tmp/boot.img -type f -exec sed -i 's/mdss_mdp.panel=1:dsi:0:qcom,mdss_dsi_g2_lgd_cmd/mdss_mdp.panel=1:dsi:0:qcom,mdss_dsi_g2_jdi_cmd/g' {} \;
-elif [ $lcdmaker == "0" ]; then
-	echo "LGD panel detected"
-	find /tmp/boot.img -type f -exec sed -i 's/mdss_mdp.panel=1:dsi:0:qcom,mdss_dsi_g2_jdi_cmd/mdss_mdp.panel=1:dsi:0:qcom,mdss_dsi_g2_lgd_cmd/g' {} \;
+elif [ "$lcdmaker" == "0" ]; then
+	echo "LGD panel detected.. Already included.."
 else
 	echo "lcd_maker_id doesn't exist. Something went wrong."
 fi
 
+#
+# Setup HH CPU max freq
+#
+
 if [ -f /data/property/persist.hh.maxfreq ]; then
 	echo "persist.hh.maxfreq already exists.. Skipping.."
 else
-	echo 0 > /data/property/persist.hh.maxfreq
+	echo -n 0 > /data/property/persist.hh.maxfreq
 	chmod 0600 /data/property/persist.hh.maxfreq
 fi
